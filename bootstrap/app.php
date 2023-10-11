9ef2e49c6709798061d2084a4c309ecb52c02907
marco-machado <marco.machado@gmail.com>
2023-10-11 18:25:59 +0000

    Fix environment loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->loadEnvironmentFrom('.env.laravel');
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
